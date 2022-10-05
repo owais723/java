618154bfd16b65dc27171f62d2888966ca69c5e9
owais723 <90831396+owais723@users.noreply.github.com>
2022-10-05 15:44:30 +0530

    Create reappear.java

---
+++ reappear.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class reappear {
+
+  
+    public static void main(String[] args) {
+        int A, B;
+	System.out.println("enter the mark in subject A : B ");
+	 Scanner sc = new Scanner(System.in);
+	 A= sc.nextInt();
+         B= sc.nextInt();
+	if (A >= 55 && B >= 45)
+	{
+		System.out.println("STUDENT QUALIFY FOR DEEGRY ");
+	}
+	else if (A >= 45 && B >= 55)
+	{
+		System.out.println("STUDENT QUALIFY FOR DEEGRY ");
+	}
+	else if (A >= 65 && B < 45)
+	{
+		System.out.println("student reapear to qualify exam B");
+	}
+	else
+		System.out.println("student is fail");
+    }
+    
+}
